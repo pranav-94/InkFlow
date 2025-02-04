39cd12ae781fa0ffcf520bec1baa8ec6d5ab0429
Pranav <ppranavvvvv918@gmail.com>
2025-02-04 23:57:15 +0530

    reponsiveness

--- src/Routes/HomePage.tsx
+++ src/Routes/HomePage.tsx
@@ -39,7 +39,7 @@ export default function HomePage() {
 
       {/* Main Content */}
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 px-4">
+        <section className="w-full py-48 md:py-24 lg:py-32 xl:py-48 px-4">
           <div className="container max-w-3xl mx-auto text-center">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
               Enhance Your Creativity
